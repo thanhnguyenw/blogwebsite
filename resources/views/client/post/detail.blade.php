2d5d91805dd9ad2eef30b0deaa648a07c18dbc28
Thanh <hoang212185@gmail.com>
2023-06-15 19:39:54 +0700

    thêm các ảnh default nếu user chưa cập nhật ảnh

--- resources/views/client/post/detail.blade.php
+++ resources/views/client/post/detail.blade.php
@@ -58,6 +58,8 @@
             <img class="rounded-circle shadow-1-strong me-3"
               src="@if ($comment->user->avatar)
               {{ asset('uploads/' . $comment->user->avatar) }}
+              @else
+              {{ asset('uploads/OIP.jfif') }}
               @endif" alt="avatar" width="40"
               height="40" />
             <div class="flex-grow-1 flex-shrink-1">
@@ -84,6 +86,8 @@
                         src="
                           @if (Auth::user()->avatar)
                           {{ asset('uploads/' . Auth::user()->avatar) }}
+                          @else
+                          {{ asset('uploads/OIP.jfif') }}
                           @endif
                         " alt="avatar" width="40"
                         height="40" />
@@ -106,7 +110,13 @@
               <div class="d-flex flex-start mt-4">
                 <a class="me-3" href="#">
                   <img class="rounded-circle shadow-1-strong"
-                    src="{{ asset('uploads/' . $reply->user->avatar) }}" alt="avatar" width="40"
+                    src="
+                    @if ($reply->user->avatar)
+                    {{ asset('uploads/' . $reply->user->avatar) }}
+                    @else
+                    {{ asset('uploads/OIP.jfif') }}
+                    @endif
+                    " alt="avatar" width="40"
                     height="40" />
                 </a>
                 <div class="flex-grow-1 flex-shrink-1">
@@ -140,6 +150,8 @@
         src="
         @if (Auth::check() && Auth::user()->avatar)
         {{ asset('uploads/' . Auth::user()->avatar) }}
+        @else
+        {{ asset('uploads/OIP.jfif') }}
         @endif
       " alt="avatar" width="40"
           height="40" />
